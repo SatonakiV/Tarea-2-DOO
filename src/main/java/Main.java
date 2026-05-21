9f7ebd95c0f9db0f19bcb74e293c2b1e3afd4a5f
Martin Labarca <labarcarojas.m@gmail.com>
2026-05-21 04:35:50 -0400

    ÃAgrega clase Main como entorno de pruebas

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("=== INICIO DE PRUEBAS TAREA 2 ===");
+
+        // Aquí iremos instanciando los objetos a medida que los programemos.
+
+
+        System.out.println("Si ves este mensaje, tu entorno de pruebas está configurado correctamente.");
+    }
+}
