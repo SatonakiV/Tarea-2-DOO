5ba5d24fb5d15ee5691671137ce6c5cb06f24b46
SatonakiV <difuentes2025@udec.cl>
2026-05-22 15:00:29 -0400

    Agregacion de clase generarTxt para generar el informe de reunion con notas de esta

---
+++ src/main/java/Informe.java
@@ -0,0 +1,44 @@
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class Informe {
+
+    public void generarTxt(Reunion reunion){
+
+        String texto = reunion.toString();
+        ArrayList<Nota> contenido = reunion.getNotas();
+
+
+        try(FileWriter Informe_Reunion = new FileWriter("Informe_reunion.txt")){
+            Informe_Reunion.write(texto);
+
+            Informe_Reunion.write("\n Notas de la reunion: ");
+
+            for(Nota n: contenido){
+                Informe_Reunion.write("\n - " + n.toString());
+            }
+
+            System.out.println("El archivo fue creado con exito");
+        }
+
+        catch(FileNotFoundException e){
+            System.err.println("Error: Ubicacion del archivo no encontrada");
+        }
+
+        catch(IOException e){
+            System.err.println("Error al escribir el informe");
+        }
+
+        catch(Exception e){
+            System.err.println("Sucedio algo inesperado, intentalo denuevo...");
+        }
+
+
+
+    }
+
+
+
+}
