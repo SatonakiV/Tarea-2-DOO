9579eed89e7346364679ed8aa73ae30c34aa07ab
Gerhec <gerhecp@gmail.com>
2026-05-23 20:23:48 -0400

    javadoc

--- src/main/java/Invitable.java
+++ src/main/java/Invitable.java
@@ -1,3 +1,9 @@
+/**
+ * Interfaz para representar a cualquier persona o grupo que pueda ser invitado a una reunion.
+ */
 public interface Invitable {
+    /**
+     * Metodo que simula el envio de una invitacion.
+     */
     public void invitar();
 }
